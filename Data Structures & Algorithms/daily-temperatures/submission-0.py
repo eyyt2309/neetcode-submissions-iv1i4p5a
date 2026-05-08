64780dbabebea4b3194d2043c50ba936b2a265d5
Ethan Yew <ethan97590773@gmail.com>
2026-05-08 18:21:45 +0200

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0 for _ in range(len(temperatures))]
+
+        stack = deque()
+
+        for idx, temperature in enumerate(temperatures):
+            if not stack:
+                stack.append((idx, temperature))
+            if stack:
+                while stack and temperature > stack[-1][1]:
+                    pos, _ = stack.pop()
+                    result[pos] = idx - pos
+                else:
+                    stack.append((idx, temperature))
+        
+
+        return result

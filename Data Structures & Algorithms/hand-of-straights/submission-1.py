df9a85fd08a649a426386bbe52d5f8856cf05f92
Ethan Yew <ethan97590773@gmail.com>
2026-05-17 14:08:06 +0200

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        hand.sort()
+        hashmap = {}
+
+        for num in hand:
+            hashmap[num] = hashmap.get(num, 0) + 1
+        
+        for num in hand:
+            if hashmap.get(num, 0) >= 1:
+                count = 1
+                hashmap[num] -= 1
+                while count != groupSize:
+                    if hashmap.get(num + 1, 0) >= 1:
+                        hashmap[num + 1] -= 1
+                        num += 1
+                        count += 1
+                    else:
+                        return False
+            else:
+                continue
+
+        return True

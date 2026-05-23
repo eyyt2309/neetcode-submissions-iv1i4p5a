0328111d924d9a00c9d3028a965ab95a3dea9042
Ethan Yew <ethan97590773@gmail.com>
2026-05-23 20:24:18 +0200

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        chars = {
+            str(i + 1): chr(ord('A') + i)
+            for i in range(26)
+        }
+        memo = {}
+        self.ways = 0
+
+        def dfs(new_str):
+            if len(new_str) == 0:
+                return 1
+            if new_str[0] == "0":
+                return 0
+            if new_str in memo:
+                return memo[new_str]
+
+            ways = 0
+            for char in chars:
+                char_len = len(char)
+                if new_str[0:0 + char_len] == char:
+                    ways += dfs(new_str[char_len:])
+            
+            memo[new_str] = ways
+            return ways
+        
+        return dfs(s)

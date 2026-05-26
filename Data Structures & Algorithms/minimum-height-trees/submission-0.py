79cb98327d32606f56d78f27fe55f22f787cc2bc
Ethan Yew <ethan97590773@gmail.com>
2026-05-26 17:57:01 +0200

    Add: minimum-height-trees - submission-0

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-0.py
@@ -0,0 +1,46 @@
+from collections import deque
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        edges_set = set(map(tuple, edges))
+        tree_dict = {}
+        minheight = float('inf')
+
+        def dfs(node, visited = None):
+            if visited == None:
+                visited = [0 for _ in range(n)]
+                visited[node] = 1
+            neighbors = self.getNeighbors(node, edges_set, visited)
+            if not neighbors:
+                return 0
+            height = 0
+            for neighbor in neighbors:
+                visited[neighbor] = 1
+                height = max(height, dfs(neighbor, visited) + 1)
+            return height
+
+        for node in range(n):
+            print("Node: ",node)
+            node_height = dfs(node)
+            print("Height: ",node_height)
+            tree_dict[node] = node_height
+            minheight = node_height if node_height < minheight else minheight
+
+        ans = []
+        for node in tree_dict:
+            if tree_dict[node] == minheight:
+                ans.append(node)
+
+        return ans
+
+    # get neighbors that have not been visited
+    def getNeighbors(self, node, edges_set, visited):
+        neighbor = []
+        for u, v in edges_set:
+            if u == node:
+                if visited[v] == 0:
+                    neighbor.append(v)
+            elif v == node:
+                if visited[u] == 0:
+                    neighbor.append(u)
+        return neighbor
+        

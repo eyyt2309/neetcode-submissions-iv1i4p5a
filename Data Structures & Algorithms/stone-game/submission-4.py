0ea195768448342f6b1fc48bbdc440f412a293f3
Ethan Yew <ethan97590773@gmail.com>
2026-05-27 15:09:14 +0200

    Add: stone-game - submission-4

---
+++ Data Structures & Algorithms/stone-game/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        memo = {}
+        def dfs(turn, piles):
+            if len(piles) == 0:
+                return 0
+
+            if (turn, tuple(piles)) in memo:
+                return memo[(turn, tuple(piles))]
+
+            front = piles[0]
+            end = piles[-1]
+
+            if turn % 2 == 0: # alice turn
+                memo[(turn, tuple(piles))] = max(dfs(turn + 1, piles[1:]) + front, dfs(turn + 1, piles[:-1]) + end)
+            elif turn % 2 == 1: #bob turn
+                memo[(turn, tuple(piles))] = min(dfs(turn + 1, piles[1:]), dfs(turn + 1, piles[:-1]))
+
+            return memo[(turn, tuple(piles))]
+
+        dfs(0, piles)
+        print(memo[(0, tuple(piles))])
+        total = sum(piles)
+        bob = total - memo[(0, tuple(piles))]
+
+        if memo[(0, tuple(piles))] > bob:
+            return True
+        else:
+            return False
+
+

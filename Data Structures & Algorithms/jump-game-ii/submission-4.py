04648ee5c2cd59970b4482272f132cf8676c5ecd
Ethan Yew <ethan97590773@gmail.com>
2026-06-10 12:15:47 +0800

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        memo = {}
+
+        def dfs(pos):
+            if pos == len(nums) - 1:
+                return 0
+            elif pos >= len(nums):
+                return float('inf')
+
+            if pos in memo:
+                return memo[pos]
+
+            min_jump = float('inf')
+            for j in range(nums[pos], 0, - 1):
+                if j == 0:
+                    continue
+                else:
+                    min_jump = min(dfs(pos + j) + 1, min_jump)
+
+            memo[pos] = min_jump
+
+            return memo[pos]
+
+        return dfs(0)

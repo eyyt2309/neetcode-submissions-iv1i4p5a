adc3e0f27d5a2f5dd462a2726ffa1048698ffb3a
Ethan Yew <ethan97590773@gmail.com>
2026-09-17 20:49:37 +0800

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = set()
+        digits = [0 for _ in range(n)]
+
+        def getCombination(nums, arr, start):
+            # if 1 combination is obtained, return
+            if nums == k:
+                ans.add(tuple(arr))
+                return
+            
+            for i in range(start, n):
+                if digits[i] == 0: # current digit not used in combination
+                    arr.append(i + 1)
+                    digits[i] = 1
+                    getCombination(nums + 1, arr, i)
+                    digits[i] = 0
+                    arr.pop()
+        arr = []
+        getCombination(0, arr, 0)
+        return [list(t) for t in ans]
+
+
+            

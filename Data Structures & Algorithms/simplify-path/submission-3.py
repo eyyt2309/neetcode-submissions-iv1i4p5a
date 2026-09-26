29678bb4a1440d260bae4bdc8089bf8d53d35292
Ethan Yew <ethan97590773@gmail.com>
2026-09-26 14:32:06 +0800

    Add: simplify-path - submission-3

---
+++ Data Structures & Algorithms/simplify-path/submission-3.py
@@ -0,0 +1,16 @@
+from collections import deque
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        parts = path.split("/")
+        stack = deque()
+
+        for part in parts:
+            if part == '..':
+                if stack:
+                    stack.pop()
+            elif part != '' and part != '.':
+                stack.append(part)
+
+        return "/" + "/".join(stack)
+
+
